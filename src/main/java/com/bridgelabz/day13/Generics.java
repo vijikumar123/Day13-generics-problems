5d9194f377ea1f312041c7e9fb77f5f54ce43a5e
vijikumar123 <vk9164940756@gmail.com>
2023-02-01 10:38:41 +0530

    added: Refactor2 generics problems

--- src/main/java/com/bridgelabz/day13/Generics.java
+++ src/main/java/com/bridgelabz/day13/Generics.java
@@ -1,29 +1,42 @@
 package com.bridgelabz.day13;
 
-public class Generics  {
-    static <P extends Comparable> void max_variables( P str1, P str2, P str3){
+public class Generics  <T extends Comparable> {
+    T var1;
+    T var2;
+    T vare3;
 
-        if(str1.compareTo(str2) > 0){
-            if(str1.compareTo(str3) > 0){
-                System.out.println( str1 + " is Maximum of three given inputs ");
+    Generics(T var1, T var2, T var3){
+        this.var1 = var1;
+        this.var2 = var2;
+        this.vare3 = var3;
+    }
+
+    void max_variables(){
+        if(this.var1.compareTo(this.var2) > 0){
+            if(this.var1.compareTo(this.vare3) > 0){
+                System.out.println( this.var1 + " is Maximum of three variables");
             }else {
-                System.out.println( str3 + " is Maximum of three given inputs  ");
+                System.out.println( this.vare3 + " is Maximum of three variables ");
             }
         }else{
-            if(str2.compareTo(str3) > 0){
-                System.out.println( str2 + " is Maximum of three given inputs  ");
+            if(this.var2.compareTo(this.vare3) > 0){
+                System.out.println( this.var2 + " is Maximum of three variables ");
             }else{
-                System.out.println( str3 + " is Maximum of three given inputs  ");
+                System.out.println( this.vare3 + " is Maximum of three variables ");
             }
         }
     }
-
-
     public static void main(String[] args) {
-        max_variables(10,5000,99);
-        max_variables(7.1f,119.8f,3.0f);
-        max_variables("Orange", "WaterMelon", "Banana");
+        Generics <Integer> obj1 = new Generics(60, 1, 45);
+        obj1.max_variables();
+
+        Generics <String> obj2 = new Generics("Orange", "Banana","WaterMelon");
+        obj2.max_variables();
+
+        Generics <Float> obj3 = new Generics(5.3f, 7.1f, 10.7f);
+        obj3.max_variables();
     }
 }
+
 
 
