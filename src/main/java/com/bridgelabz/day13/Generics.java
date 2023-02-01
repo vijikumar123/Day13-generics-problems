387d029ac83c7486eb0aaa7706a647466f58ecd9
vijikumar123 <vk9164940756@gmail.com>
2023-02-01 10:47:43 +0530

    added: uc2 generics problems

--- src/main/java/com/bridgelabz/day13/Generics.java
+++ src/main/java/com/bridgelabz/day13/Generics.java
@@ -1,26 +1,28 @@
 package com.bridgelabz.day13;
 
 public class Generics  {
-    static void max_variables(Integer num1, Integer num2, Integer num3){
+    static void max_variables(Float num1, Float num2, Float num3){
         if(num1.compareTo(num2) > 0){
             if(num1.compareTo(num3) > 0){
-                System.out.println( num1 + " is Maximum of three numbers ");
+                System.out.println( num1 + " is Maximum of three float numbers ");
             }else {
-                System.out.println( num3 + " is Maximum of three numbers ");
+                System.out.println( num3 + " is Maximum of three float numbers ");
             }
         }else{
             if(num2.compareTo(num3) > 0){
-                System.out.println( num2 + " is Maximum of three numbers ");
+                System.out.println( num2 + " is Maximum of three float numbers ");
             }else{
-                System.out.println( num3 + " is Maximum of three numbers ");
+                System.out.println( num3 + " is Maximum of three float numbers ");
             }
         }
     }
 
     public static void main(String[] args) {
-        max_variables(345,445,1000);
+        max_variables(1.3f,1.5f,0.2f);
     }
 }
+
+
 
 
 
