0b86d10f05f00724f18b231b2850cb229ab41577
vijikumar123 <vk9164940756@gmail.com>
2023-02-01 10:51:37 +0530

    added: uc3 generics problems

--- src/main/java/com/bridgelabz/day13/Generics.java
+++ src/main/java/com/bridgelabz/day13/Generics.java
@@ -1,6 +1,37 @@
 package com.bridgelabz.day13;
 
 public class Generics  {
+    static void max_variables(String str1, String str2, String str3){
+        if(str1.compareTo(str2) > 0){
+            if(str1.compareTo(str3) > 0){
+                System.out.println( str1 + " is Maximum of three strings ");
+            }else {
+                System.out.println( str3 + " is Maximum of three strings ");
+            }
+        }else{
+            if(str2.compareTo(str3) > 0){
+                System.out.println( str2 + " is Maximum of three strings ");
+            }else{
+                System.out.println( str3 + " is Maximum of three strings ");
+            }
+        }
+    }
+    static void max_variables(Integer num1, Integer num2, Integer num3){
+        if(num1.compareTo(num2) > 0){
+            if(num1.compareTo(num3) > 0){
+                System.out.println( num1 + " is Maximum of three integers ");
+            }else {
+                System.out.println( num3 + " is Maximum of three integers ");
+            }
+        }else{
+            if(num2.compareTo(num3) > 0){
+                System.out.println( num2 + " is Maximum of three integers ");
+            }else{
+                System.out.println( num3 + " is Maximum of three integers ");
+            }
+        }
+    }
+
     static void max_variables(Float num1, Float num2, Float num3){
         if(num1.compareTo(num2) > 0){
             if(num1.compareTo(num3) > 0){
@@ -18,9 +49,13 @@ public class Generics  {
     }
 
     public static void main(String[] args) {
-        max_variables(1.3f,1.5f,0.2f);
+        max_variables(10,500,99);
+        max_variables(6.1f,1.8f,70f);
+        max_variables("Orange", "WaterMelon", "Banana");
     }
 }
+
+
 
 
 
