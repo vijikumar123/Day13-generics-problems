1ae7b771122297d0b3ad89781ad714d388d6297a
vijikumar123 <vk9164940756@gmail.com>
2023-02-01 10:43:21 +0530

    added: uc1 generics problems

--- src/main/java/com/bridgelabz/day13/Generics.java
+++ src/main/java/com/bridgelabz/day13/Generics.java
@@ -1,42 +1,27 @@
 package com.bridgelabz.day13;
 
-public class Generics  <T extends Comparable> {
-    T var1;
-    T var2;
-    T vare3;
-
-    Generics(T var1, T var2, T var3){
-        this.var1 = var1;
-        this.var2 = var2;
-        this.vare3 = var3;
-    }
-
-    void max_variables(){
-        if(this.var1.compareTo(this.var2) > 0){
-            if(this.var1.compareTo(this.vare3) > 0){
-                System.out.println( this.var1 + " is Maximum of three variables");
+public class Generics  {
+    static void max_variables(Integer num1, Integer num2, Integer num3){
+        if(num1.compareTo(num2) > 0){
+            if(num1.compareTo(num3) > 0){
+                System.out.println( num1 + " is Maximum of three numbers ");
             }else {
-                System.out.println( this.vare3 + " is Maximum of three variables ");
+                System.out.println( num3 + " is Maximum of three numbers ");
             }
         }else{
-            if(this.var2.compareTo(this.vare3) > 0){
-                System.out.println( this.var2 + " is Maximum of three variables ");
+            if(num2.compareTo(num3) > 0){
+                System.out.println( num2 + " is Maximum of three numbers ");
             }else{
-                System.out.println( this.vare3 + " is Maximum of three variables ");
+                System.out.println( num3 + " is Maximum of three numbers ");
             }
         }
     }
+
     public static void main(String[] args) {
-        Generics <Integer> obj1 = new Generics(60, 1, 45);
-        obj1.max_variables();
-
-        Generics <String> obj2 = new Generics("Orange", "Banana","WaterMelon");
-        obj2.max_variables();
-
-        Generics <Float> obj3 = new Generics(5.3f, 7.1f, 10.7f);
-        obj3.max_variables();
+        max_variables(345,445,1000);
     }
 }
+
 
 
 
